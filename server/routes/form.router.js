3bd625d650b61e2ee29fc7c38174548ad990d22d
Al Camara <Alhousseinec@gmail.com>
2023-02-14 21:37:51 -0600

    import msgTemplates.json to server

--- server/routes/form.router.js
+++ server/routes/form.router.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
+//imported json files
 const guests = require("../../public/Guests.json");
 const companies = require("../../public/Companies.json");
+const msgTemplates = require("../../public/MsgTemplates.json");
 
 router.get("/form", (req, res) => {
   const guestList = [];
   const companiesList = [];
+  const msgCategoryList = [];
+
+  console.log(msgTemplates);
 
   for (let guest of guests) {
     guestList.push({
@@ -23,7 +28,18 @@ router.get("/form", (req, res) => {
     });
   }
 
-  res.send({ guestList: guestList, companiesList: companiesList });
+  for (let msg of msgTemplates) {
+    msgCategoryList.push({
+      id: msg.id,
+      category: msg.category,
+    });
+  }
+
+  res.send({
+    guests: guestList,
+    companies: companiesList,
+    category: msgCategoryList,
+  });
 });
 
 module.exports = router;
