8cb12d4d6e7c6d0359ac1eb84701e4f8e9c62bd5
Al Camara <Alhousseinec@gmail.com>
2023-02-14 21:04:07 -0600

    create and send object company and guest info

--- server/routes/form.router.js
+++ server/routes/form.router.js
@@ -5,8 +5,25 @@ const guests = require("../../public/Guests.json");
 const companies = require("../../public/Companies.json");
 
 router.get("/form", (req, res) => {
-  console.log(guests);
-  res.send(companies);
+  const guestList = [];
+  const companiesList = [];
+
+  for (let guest of guests) {
+    guestList.push({
+      id: guest.id,
+      firstName: guest.firstName,
+      lastName: guest.lastName,
+    });
+  }
+
+  for (let company of companies) {
+    companiesList.push({
+      id: company.id,
+      name: company.company,
+    });
+  }
+
+  res.send({ guestList: guestList, companiesList: companiesList });
 });
 
 module.exports = router;
